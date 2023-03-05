3cf0f5a1821c005f3c22421d091bb32239424b69
eunhye <seomne032@gamil.com>
2023-03-05 11:55:46 +0900

    magnific_popup

---
+++ 17.magnific_popup/app.js
@@ -0,0 +1,88 @@
+$(function () {
+    //open modal
+    $('.popup-modal').magnificPopup({
+        type: 'inline',
+        preloader: false,
+        focus: '#username',
+        modal: true,
+    });
+    $(document).on('click', '.popup-modal-dismiss', function (e) {
+        e.preventDefault();
+        $.magnificPopup.close();
+    });
+
+    //youtube
+    $('.popup-youtube').magnificPopup({
+        disableOn: 700,
+        type: 'iframe',
+        mainClass: 'mfp-fade',
+        removalDelay: 160,
+        preloader: false,
+
+        fixedContentPos: false,
+    });
+
+    //load ajax
+    $('.simple-ajax-popup-align-top').magnificPopup({
+        type: 'ajax',
+        alignTop: true,
+        overflowY: 'scroll', // as we know that popup content is tall we set scroll overflow by default to avoid jump
+    });
+
+    $('.simple-ajax-popup').magnificPopup({
+        type: 'ajax',
+    });
+
+    //single lightbox
+    $('.image-popup-vertical-fit').magnificPopup({
+        type: 'image',
+        closeOnContentClick: true,
+        mainClass: 'mfp-img-mobile',
+        image: {
+            verticalFit: true,
+        },
+    });
+    $('.image-popup-fit-width').magnificPopup({
+        type: 'image',
+        closeOnContentClick: true,
+        image: {
+            verticalFit: false,
+        },
+    });
+    $('.image-popup-no-margins').magnificPopup({
+        type: 'image',
+        closeOnContentClick: true,
+        closeBtnInside: false,
+        fixedContentPos: true,
+        mainClass: 'mfp-no-margins mfp-with-zoom', // class to remove default margin from left and right side
+        image: {
+            verticalFit: true,
+        },
+        zoom: {
+            enabled: true,
+            duration: 300, // don't foget to change the duration also in CSS
+        },
+    });
+
+    //gallery
+    $('.gallery').magnificPopup({
+        delegate: 'a',
+        type: 'image',
+        tLoading: 'Loading image #%curr%...',
+        mainClass: 'mfp-img-mobile',
+        gallery: {
+            enabled: true,
+            navigateByImgClick: true,
+            preload: [0, 1], // Will preload 0 - before current, and 1 after the current image
+        },
+        image: {
+            tError: '<a href="%url%">The image #%curr%</a> could not be loaded.',
+            titleSrc: function (item) {
+                return (
+                    item.el.attr('title') +
+                    '<small>by Marsel Van Oosten</small>'
+                );
+            },
+        },
+    });
+});
